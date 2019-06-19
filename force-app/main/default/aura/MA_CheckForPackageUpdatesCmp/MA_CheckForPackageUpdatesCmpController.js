fe5422724e6c353fdd8eca2cc497c6975fc4d6e0
Doug Ayers <douglascayers@gmail.com>
2019-06-18 21:27:04 -0500

    update to v46.0 (summer '19)

--- force-app/main/default/aura/MA_CheckForPackageUpdatesCmp/MA_CheckForPackageUpdatesCmpController.js
+++ force-app/main/default/aura/MA_CheckForPackageUpdatesCmp/MA_CheckForPackageUpdatesCmpController.js
@@ -16,7 +16,7 @@
         // get the currently installed package version
         promises.push(
             lcApi.restRequest({
-                'url' : '/services/data/v45.0/tooling/query?q=SELECT+Id,+SubscriberPackageId,+SubscriberPackage.Name,+SubscriberPackage.NamespacePrefix,+SubscriberPackageVersion.MajorVersion,+SubscriberPackageVersion.MinorVersion+FROM+InstalledSubscriberPackage'
+                'url' : '/services/data/v46.0/tooling/query?q=SELECT+Id,+SubscriberPackageId,+SubscriberPackage.Name,+SubscriberPackage.NamespacePrefix,+SubscriberPackageVersion.MajorVersion,+SubscriberPackageVersion.MinorVersion+FROM+InstalledSubscriberPackage'
             }).then( function ( response ) {
                 // The InstalledSubscriberPackage object doesn't support WHERE clause filtering on the package's namespace
                 // so we have to filter the results ourselves
